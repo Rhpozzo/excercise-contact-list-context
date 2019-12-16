c78127f7f8033252849133a95995f7bf0f133d42
Román Pozzo <rhpozzo@gmail.com>
2019-12-16 01:16:47 +0000

    fetch added

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,40 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contact: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			fetchdata() {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/whatever")
+					.then(data => {
+						return data.json();
+					})
+					.then(res => {
+						setStore({
+							...setStore,
+							contact: res
+						});
+					})
+					.catch(e => console.log(e));
+			}
+			// postNewAgenda(name) {
+			// 	fetch("https://assets.breatheco.de/apis/fake/contact/", {
+			// 		method: "POST",
+			// 		headers: new Headers(),
+			// 		body: {
+			// 			full_name: "Nimitz",
+			// 			email: "nimitz@destroyer.com",
+			// 			agenda_slug: "Nimitz",
+			// 			address: ""
+			// 		}
+			// 	})
+			// 		.then(res => res.json())
+			// 		.then(data => console.log(data))
+			// 		.catch(err => console.log(err));
+			// }
 		}
 	};
 };
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { Context } from "../store/appContext";
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
@@ -8,6 +8,7 @@ export const Contacts = () => {
 	const [state, setState] = useState({
 		showModal: false
 	});
+	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
@@ -19,10 +20,19 @@ export const Contacts = () => {
 				</p>
 				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => setState({ showModal: true })} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
+						{store.contact.map((item, index) => {
+							return (
+								<div key={item.id}>
+									<ContactCard
+										onDelete={() => setState({ showModal: true })}
+										full_name={item.full_name}
+										email={item.email}
+										phone={item.phone}
+										address={item.address}
+									/>
+								</div>
+							);
+						})}
 					</ul>
 				</div>
 			</div>
